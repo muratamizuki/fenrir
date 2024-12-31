f1c34f5309552765b92125f14d975f2768a77da0
mizuki murata <mizucmke@gmail.com>
2024-12-31 19:13:08 +0900

    フロント環境構築

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleCheckbox from './components/checkbox';
+
+function App() {
+  return (
+    <div>
+      <SimpleCheckbox/>
+    </div>
+  );
+}
+
+export default App;
