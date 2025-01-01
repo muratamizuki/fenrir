1a059cb7697b8d018abf017fca28da5eb14c78b5
mizuki murata <mizucmke@gmail.com>
2025-01-01 18:44:17 +0900

    .envとrepuirementsを編集追加

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+# デバック用のエンドポイント
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    selected_labels = data.get('selectedLabels', [])
+    additional_info = data.get('additionalInfo', '')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print("受信したデータ:")
+    print("選択されたラベル:", selected_labels)
+    print("追加情報:", additional_info)
+
+    return jsonify({"message": "データが正常に受信されました"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
